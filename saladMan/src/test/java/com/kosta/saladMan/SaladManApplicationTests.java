8978689087fd970237d5ca4305000240d9aebae5
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-06-11 22:59:19 +0900

    [test]CI/CD 테스트중

--- saladMan/src/test/java/com/kosta/saladMan/SaladManApplicationTests.java
+++ saladMan/src/test/java/com/kosta/saladMan/SaladManApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SaladManApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
