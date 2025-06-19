5b8ce5198d415c6c9e9328df9e66e055663c6f4c
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-06-20 07:56:02 +0900

    [docs] 헬스 jwt필터 제외

--- saladMan/src/main/java/com/kosta/saladMan/config/SecurityConfig.java
+++ saladMan/src/main/java/com/kosta/saladMan/config/SecurityConfig.java
@@ -103,9 +103,10 @@ public class SecurityConfig {
 				.addFilterAt(new JwtAuthenticationFilter(authenticationManager),
 						UsernamePasswordAuthenticationFilter.class);
 
-		http.addFilter(new JwtAuthorizationFilter(authenticationManager, storeRepository)).authorizeRequests()
-//	.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")//로그인 필수 && 권한이 ADMIN이거나 MANAGER 만 허용 
-//	.antMatchers("/manager/**").access("hasRole('ROLE_MANAGER')")//로그인 필수 && 권한이 MANAGER 만 허용
+		http.addFilter(new JwtAuthorizationFilter(authenticationManager, storeRepository))
+				.authorizeRequests()
+			//	.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")//로그인 필수 && 권한이 ADMIN이거나 MANAGER 만 허용 
+			//	.antMatchers("/manager/**").access("hasRole('ROLE_MANAGER')")//로그인 필수 && 권한이 MANAGER 만 허용
 				.antMatchers("/actuator/health").permitAll() // ✅ actuator 허용
 				.anyRequest().permitAll();
 		return http.build();
