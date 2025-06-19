6cf055c623aeab3a496f222bb6feeae92feffeef
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-06-19 11:21:36 +0900

    [docs] ngnix적용으로 인한 외부포트 변경

--- saladMan/scripts/start-server.sh
+++ saladMan/scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop saladman-server || true
-docker rm saladman-server || true
-docker pull 362717844282.dkr.ecr.ap-northeast-2.amazonaws.com/saladman-server:latest
-docker run -d --name saladman-server -p 8090:8090 362717844282.dkr.ecr.ap-northeast-2.amazonaws.com/saladman-server:latest
+sudo docker stop saladman-server || true
+sudo docker rm saladman-server || true
+sudo docker pull 362717844282.dkr.ecr.ap-northeast-2.amazonaws.com/saladman-server:latest
+sudo docker run -d --name saladman-server -p 80:8090 362717844282.dkr.ecr.ap-northeast-2.amazonaws.com/saladman-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
