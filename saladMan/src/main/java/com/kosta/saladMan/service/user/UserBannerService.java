b86805958adeb7e138d05f82028134c89193c88e
jhzzng <jhzzng01@gmail.com>
2025-06-30 17:34:27 +0900

    [create] user 메인페이지  사진 수정하기 기능 추가
    hq로 로그인 해야 수정하기 기능 나옴

--- saladMan/src/main/java/com/kosta/saladMan/service/user/UserBannerService.java
+++ saladMan/src/main/java/com/kosta/saladMan/service/user/UserBannerService.java
@@ -1,8 +1,11 @@
 package com.kosta.saladMan.service.user;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.kosta.saladMan.entity.userBanner.UserBanner;
 
 public interface UserBannerService {
     UserBanner getBanner(Long id);
+
     UserBanner updateBanner(Long id, UserBanner dto);
 }
--- saladMan/src/main/java/com/kosta/saladMan/service/user/UserBannerServiceImpl.java
+++ saladMan/src/main/java/com/kosta/saladMan/service/user/UserBannerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.kosta.saladMan.service.user;
 
-import org.springframework.stereotype.Service;
-
+import com.kosta.saladMan.controller.common.S3Uploader;
 import com.kosta.saladMan.entity.userBanner.UserBanner;
 import com.kosta.saladMan.repository.user.UserBannerRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,19 +18,18 @@ public class UserBannerServiceImpl implements UserBannerService {
     @Override
     public UserBanner getBanner(Long id) {
         return userBannerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("배너가 존재하지 않습니다."));
+            .orElseThrow(() -> new IllegalArgumentException("배너가 존재하지 않습니다."));
     }
 
     @Override
     public UserBanner updateBanner(Long id, UserBanner dto) {
         UserBanner banner = userBannerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("배너가 존재하지 않습니다."));
-
+            .orElseThrow(() -> new IllegalArgumentException("배너가 존재하지 않습니다."));
         banner.setLine1(dto.getLine1());
         banner.setLine2(dto.getLine2());
         banner.setLine3(dto.getLine3());
         banner.setImage(dto.getImage());
-
         return userBannerRepository.save(banner);
     }
 }
+
