9b0ec940a81ffaa18348a6023833a734f2929784
jeun10 <luluka11@naver.com>
2025-07-04 09:18:00 +0900

    [Docs] 키오스크 설정 변경

--- saladMan/src/main/java/com/kosta/saladMan/config/CorsConfig.java
+++ saladMan/src/main/java/com/kosta/saladMan/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.kosta.saladMan.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,8 +33,14 @@ public class CorsConfig implements WebMvcConfigurer {
 		config.addAllowedOrigin("http://localhost:5173");
 		config.addAllowedOrigin("http://saladman-web.s3-website.ap-northeast-2.amazonaws.com");
         config.addAllowedOrigin("https://api.saladman.com");
+        config.addAllowedOrigin("http://localhost:8081");
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://192.168.0.9:8080");
+        config.addAllowedOrigin("http://192.168.0.9:5173");
 
-		config.addAllowedOriginPattern("*");
+
+
+//		config.addAllowedOriginPattern("*");
 		config.addAllowedHeader("*"); //프론트의 Access-Control-Allow-Headers 요청에 대한 허용
 		config.addAllowedMethod("*"); //프론트의 Access-Control-Allow-Method 요청에 대한 허용
 		config.addExposedHeader(JwtProperties.HEADER_STRING); //클라이언트(리액트 등)가 응답에 접간할 수 있는 Header 추가
@@ -41,4 +49,26 @@ public class CorsConfig implements WebMvcConfigurer {
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
+    
+    @Bean
+    public CorsFilter kioskCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "http://192.168.0.23:5173",
+            "http://localhost:8081",
+            "http://saladman-web.s3-website.ap-northeast-2.amazonaws.com",
+            "https://api.saladman.com",
+            "http://192.168.0.9:8080",
+            "http://192.168.0.9:5173"
+
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/kiosk/**", config);
+        return new CorsFilter(source);
+    }
 }
