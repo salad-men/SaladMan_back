c1c33d3a869e350b06aac730e00d833791c40c78
Gido Park <111720411+prayer0420@users.noreply.github.com>
2025-07-14 11:34:00 +0900

    [docs] 매장대시보드 수정

--- saladMan/src/main/java/com/kosta/saladMan/service/order/OrderService.java
+++ saladMan/src/main/java/com/kosta/saladMan/service/order/OrderService.java
@@ -75,6 +75,7 @@ public interface OrderService {
     //-----------------------대시보드---------------------------
 	int getAutoOrderExpectedCount(Integer storeId);
     OrderSummaryDto getOrderSummaryTop3WithCountMerged(String startDate, String endDate);
+    Map<String, Integer> getOrderStatusCountByStore(Integer storeId, String startDate, String endDate);
 
 } 
 
